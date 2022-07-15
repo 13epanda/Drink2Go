b25ab76342b1f9b0192a2800d93d3025c1c6cc1e
13epanda <veratihonova95@gmail.com>
2022-07-15 15:12:43 +0500

    added swiper

---
+++ source/js/slider.js
@@ -0,0 +1,30 @@
+// core version + navigation, pagination modules:
+import Swiper from 'https://unpkg.com/swiper@8/swiper-bundle.esm.browser.min.js'
+//import Swiper, { Navigation, Pagination } from 'swiper';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.page-main__main-slider', {
+  // Optional parameters
+  //direction: 'vertical',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
